d562344e64662dcad19fb0e6e5ead58d9097baee
blobcode <lhelme123@gmail.com>
2025-05-13 16:47:20 -0400

    requirements

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash
 import os
 
 from pathing import plan_course_schedule_cp, get_semester_type
